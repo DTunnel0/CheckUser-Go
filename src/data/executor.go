8bf0e6d50209ad2da7e5f26642bfe5155306ea17
DTunnel0 <glemison.dutra09@gmail.com>
2024-02-05 02:08:50 -0300

    added cache

--- src/data/executor.go
+++ src/data/executor.go
@@ -4,22 +4,63 @@ import (
 	"context"
 	"os/exec"
 	"strings"
+	"sync"
+	"time"
 
 	"github.com/DTunnel0/CheckUser-Go/src/domain/contract"
 )
 
 type bashExecutor struct {
+	cache map[string]*cacheItem
+	mutex sync.Mutex
+}
+
+type cacheItem struct {
+	expiry time.Time
+	value  string
 }
 
 func NewBashExecutor() contract.Executor {
-	return &bashExecutor{}
+	return &bashExecutor{
+		cache: make(map[string]*cacheItem),
+	}
+}
+
+func (b *bashExecutor) getCachedResult(command string) (string, bool) {
+	b.mutex.Lock()
+	cachedItem, ok := b.cache[command]
+	b.mutex.Unlock()
+
+	if ok && time.Now().Before(cachedItem.expiry) {
+		return cachedItem.value, true
+	}
+
+	return "", false
 }
 
 func (b *bashExecutor) Execute(ctx context.Context, command string) (string, error) {
+	ctx, cancel := context.WithTimeout(ctx, time.Second)
+	defer cancel()
+
+	cachedResult, ok := b.getCachedResult(command)
+	if ok {
+		return cachedResult, nil
+	}
+
 	cmd := exec.CommandContext(ctx, "bash", "-c", command)
-	result, err := cmd.Output()
+	output, err := cmd.Output()
 	if err != nil {
 		return "", err
 	}
-	return strings.TrimSpace(string(result)), nil
+
+	result := strings.TrimSpace(string(output))
+
+	b.mutex.Lock()
+	b.cache[command] = &cacheItem{
+		expiry: time.Now().Add(time.Second * 30),
+		value:  result,
+	}
+	b.mutex.Unlock()
+
+	return result, nil
 }
