fc734ea960155154a7f63e12e2e3838372853c9b
DTunnel0 <glemison.dutra09@gmail.com>
2024-02-05 00:14:05 -0300

    up

--- src/data/executor.go
+++ src/data/executor.go
@@ -1,25 +1,38 @@
 package data
 
 import (
+	"bytes"
 	"context"
 	"os/exec"
-	"strings"
 
 	"github.com/DTunnel0/CheckUser-Go/src/domain/contract"
 )
 
 type bashExecutor struct {
+	pool chan *exec.Cmd
 }
 
 func NewBashExecutor() contract.Executor {
-	return &bashExecutor{}
+	poolSize := 100
+	pool := make(chan *exec.Cmd, poolSize)
+	for i := 0; i < poolSize; i++ {
+		pool <- exec.Command("bash")
+	}
+	return &bashExecutor{pool: pool}
 }
 
 func (b *bashExecutor) Execute(ctx context.Context, command string) (string, error) {
-	cmd := exec.CommandContext(ctx, "bash", "-c", command)
-	result, err := cmd.Output()
+	cmd := <-b.pool
+	defer func() {
+		b.pool <- cmd
+	}()
+
+	cmd.Args = []string{"bash", "-c", command}
+	var out bytes.Buffer
+	cmd.Stdout = &out
+	err := cmd.Run()
 	if err != nil {
 		return "", err
 	}
-	return strings.TrimSpace(string(result)), nil
+	return out.String(), nil
 }
