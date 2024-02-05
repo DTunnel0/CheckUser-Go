6b82ae921ad2be16f9be4ec83b799f09b329d3ac
DTunnel0 <glemison.dutra09@gmail.com>
2024-02-05 15:53:59 -0300

    release

--- src/main.go
+++ src/main.go
@@ -12,7 +12,7 @@ import (
 )
 
 var (
-	version     = "0.1.4"
+	version     = "0.1.5"
 	version_str = fmt.Sprintf("checkuser %s", version)
 	author      = "Glemison C. Dutra"
 	email       = "glemyson20@gmail.com"
