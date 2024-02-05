00015f5f2ff33e86dc9c36517aab32731fce8d2a
DTunnel0 <glemison.dutra09@gmail.com>
2024-02-05 12:43:35 -0300

    up

--- src/data/executor.go
+++ src/data/executor.go
@@ -4,11 +4,21 @@ import (
 	"context"
 	"os/exec"
 	"strings"
+	"sync"
+	"time"
 
 	"github.com/DTunnel0/CheckUser-Go/src/domain/contract"
 )
 
+const cacheExpiresTime = 1 * time.Minute
+
 type bashExecutor struct {
+	cache sync.Map
+}
+
+type cachedResult struct {
+	result      string
+	lastFetched time.Time
 }
 
 func NewBashExecutor() contract.Executor {
@@ -16,12 +26,42 @@ func NewBashExecutor() contract.Executor {
 }
 
 func (b *bashExecutor) Execute(ctx context.Context, command string) (string, error) {
-	args := strings.Split(command, " ")
-	cmd := exec.CommandContext(ctx, args[0], args[1:]...)
+	if cachedResult, ok := b.getCachedResult(command); ok {
+		return cachedResult, nil
+	}
 
-	result, err := cmd.Output()
+	result, err := b.executeCommand(ctx, command)
 	if err != nil {
 		return "", err
 	}
-	return strings.TrimSpace(string(result)), nil
+
+	b.cacheResult(command, result)
+	return result, nil
+}
+
+func (b *bashExecutor) getCachedResult(command string) (string, bool) {
+	if cached, ok := b.cache.Load(command); ok {
+		if cachedResult, ok := cached.(cachedResult); ok && time.Since(cachedResult.lastFetched) < cacheExpiresTime {
+			return cachedResult.result, true
+		}
+	}
+	return "", false
+}
+
+func (b *bashExecutor) executeCommand(ctx context.Context, command string) (string, error) {
+	args := strings.Split(command, " ")
+	cmd := exec.CommandContext(ctx, args[0], args[1:]...)
+
+	resultBytes, err := cmd.Output()
+	if err != nil {
+		return "", err
+	}
+	return strings.TrimSpace(string(resultBytes)), nil
+}
+
+func (b *bashExecutor) cacheResult(command, result string) {
+	b.cache.Store(command, cachedResult{
+		result:      result,
+		lastFetched: time.Now(),
+	})
 }
