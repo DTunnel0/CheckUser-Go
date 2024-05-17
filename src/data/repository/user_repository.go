c51c48de01b403c6bca89333dd5206373fd0e0af
DTunnel0 <glemison.dutra09@gmail.com>
2024-05-17 12:49:30 -0300

    up

--- src/data/repository/user_repository.go
+++ src/data/repository/user_repository.go
@@ -2,35 +2,25 @@ package repository
 
 import (
 	"context"
-	"time"
 
 	"github.com/DTunnel0/CheckUser-Go/src/domain/contract"
 	"github.com/DTunnel0/CheckUser-Go/src/domain/entity"
 )
 
 type systemUserRepository struct {
-	userDAO          contract.UserDAO
-	userCacheService contract.UserCacheService
+	userDAO contract.UserDAO
 }
 
-func NewSystemUserRepository(userDAO contract.UserDAO, userCacheService contract.UserCacheService) contract.UserRepository {
+func NewSystemUserRepository(userDAO contract.UserDAO) contract.UserRepository {
 	return &systemUserRepository{
-		userDAO:          userDAO,
-		userCacheService: userCacheService,
+		userDAO: userDAO,
 	}
 }
 
 func (r *systemUserRepository) FindByUsername(ctx context.Context, username string) (*entity.User, error) {
-	user, found := r.userCacheService.Get(username)
-	if found {
-		return user, nil
-	}
-
 	user, err := r.userDAO.FindByUsername(ctx, username)
 	if err != nil {
 		return nil, err
 	}
-
-	r.userCacheService.Set(user, time.Minute*30)
 	return user, nil
 }
--- src/infra/factory/user.go
+++ src/infra/factory/user.go
@@ -13,9 +13,8 @@ import (
 
 func MakeCheckUserHandler() handler.Handler {
 	executor := data.NewBashExecutor()
-	userCaseService := cache.NewUserCacheService()
 	userDAO := dao.NewUserDAO(executor)
-	userRepository := repository.NewSystemUserRepository(userDAO, userCaseService)
+	userRepository := repository.NewSystemUserRepository(userDAO)
 	deviceRepository := repository.NewSQLiteDeviceRepository()
 	checkUserUseCase := user_use_case.NewCheckUserUseCase(userRepository, deviceRepository)
 	return user_handler.NewCheckUserHandler(checkUserUseCase)
@@ -32,9 +31,8 @@ func MakeCountConnectionsHandler() handler.Handler {
 
 func MakeDetailsUserHandler() handler.Handler {
 	executor := data.NewBashExecutor()
-	userCaseService := cache.NewUserCacheService()
 	userDAO := dao.NewUserDAO(executor)
-	userRepository := repository.NewSystemUserRepository(userDAO, userCaseService)
+	userRepository := repository.NewSystemUserRepository(userDAO)
 	countSSH := connection.NewSSHConnection(executor)
 	countSSH.SetNext(connection.NewOpenVPNConnection(connection.NewAUXOpenVPNConnection("127.0.0.1", 7505)))
 	checkUserUseCase := user_use_case.NewDetailUserUseCase(userRepository, countSSH)
